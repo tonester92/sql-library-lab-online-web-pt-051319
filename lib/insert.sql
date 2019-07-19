ffb52e6754f851008ebf6b305c0e4fa0beb0dd80
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 15:48:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,7 +23,7 @@ INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Ron
 
 INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Albus Dumbledore", "It matters not what someone is born, but what they grow to be.", "Half-Blood", 1, 1);
 
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("", "This is Nenya, the Ring of Adamant, and I am its keeper.", "human", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Daenerys Targaryen", "This is Nenya, the Ring of Adamant, and I am its keeper.", "human", 2, 2);
 
 INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Jon Snow", "Do not think I won't kill you, dwarf! It would be my pleasure", "human", 2, 2);
 
