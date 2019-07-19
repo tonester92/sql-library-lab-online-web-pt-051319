ef9fb12eb553fcc437f068c97c7877d314b109bb
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 16:01:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,11 +25,11 @@ def select_name_and_series_subgenres_of_authors
     INNER JOIN authors
     ON series.author_id = authors.id
     INNER JOIN subgenres
-    ON series.subgenre_id = subgenres.id;""
+    ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
