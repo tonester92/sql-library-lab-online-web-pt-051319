7acfcffb2a6ff0662eb632886d5471ef59066ae6
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 15:33:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,9 +8,9 @@ INSERT INTO Books (title, year, series_id) VALUES ("Harry Potter and the Sorcere
 INSERT INTO Books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 1);
 
 INSERT INTO Books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1);
-INSERT INTO Books (title, year, series_id) VALUES ("The Game of Thrones", 1, 2);
+INSERT INTO Books (title, year, series_id) VALUES ("The Game of Thrones", 1996, 2);
 
-INSERT INTO Books (title, year, series_id) VALUES ("Lord of the Rings: The Two Towers", 1954, 2);
+INSERT INTO Books (title, year, series_id) VALUES ("", 1954, 2);
 
 INSERT INTO Books (title, year, series_id) VALUES ("Lord of the Rings: The Return of the King", 1955, 2);
 
