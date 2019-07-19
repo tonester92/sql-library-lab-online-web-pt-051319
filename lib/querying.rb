e0dc84388658a7e2110e22155e0f11e93e2652ff
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 16:00:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,11 +14,14 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
+    ORDER BY num_species DESC
+    LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  ""
 end
 
 def select_series_title_with_most_human_characters
